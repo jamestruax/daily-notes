126ee21846308f2859cc1c09a9f142eccff42741
James Truax <jamestruax@gmail.com>
2013-11-07 20:14:44 -0500

    Removed duplicate jquery.js includes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-datepicker
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es
