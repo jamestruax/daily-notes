c92584c3ee21a6c9dbae1ecb1ab54939cd8107a4
James Truax <jamestruax@gmail.com>
2013-11-10 01:02:10 -0500

    Removed debug code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,20 +7,7 @@ class HomeController < ApplicationController
       
       @loggedInMessage = "Logged in"
       
-      #@notes = @current_user.allNotesSorted
       @allMonthlyNotes = @current_user.month_note_lists
-      
-      @dates = Set.new
-      
-      @notes = Array.new
-      @current_user.month_note_lists.each do |month|
-        dayLists = month.day_note_lists.each do |day|
-          day.notes.each do |note|
-            @notes.push( note )
-            @dates.add( note.date )
-          end
-        end
-      end
     end
   end
 end
